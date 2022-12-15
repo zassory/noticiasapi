ba92b0f0de13c800a425d21cd2ff4e736ad28d2a
Nicolas <nicolas.programador@gmail.com>
2022-12-15 18:44:49 -0300

    Formulario con grid y media query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Container , Grid , Typography } from '@mui/material';
-
+import { Formulario } from './components/Formulario';
 
 function App() {
   
@@ -11,6 +11,19 @@ function App() {
         Buscador de Noticias
       </Typography>
     </header>
+
+    <Grid 
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+      <Grid item xs={12} md={6} lg={4}>
+
+        <Formulario />
+      </Grid>
+    </Grid>    
+
    </Container>
   )
 }
