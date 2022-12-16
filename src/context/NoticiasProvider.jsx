882d20b6382c0e1a4d03a126a36f72e44e21ed70
Nicolas <nicolas.programador@gmail.com>
2022-12-16 12:37:34 -0300

    conexion a api

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
 import { useState , useEffect , createContext } from 'react';
 
 const NoticiasContext = createContext();
 
 export const NoticiasProvider = ({children}) => {
     const [categoria,setCategoria] = useState('general');
+    const [noticias,setNoticias] = useState([]);
+
+    useEffect( () => {
+        const consultarAPI = async() => {
+            const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+            const { data } = await axios(url);
+
+            setNoticias(data.articles);
+        }
+        consultarAPI();
+    }, [categoria] );
 
     const handleChangeCategoria = e => {
         setCategoria(e.target.value);
